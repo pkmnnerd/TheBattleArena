04b136400cab1722818af8d392a578eaaabb1ed8
jonzhang2 <jonzhang42@gmail.com>
2018-03-14 19:32:09 -0400

    Make FPS Dude

---
+++ The_Battle_Arena/Assets/CameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float mouseSenY;
+    public Camera cam;
+    //private float angle;
+
+	// Use this for initialization
+	void Start () {
+//        angle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void FixedUpdate()
+    {
+        float moveMouseVertical = Input.GetAxis("Mouse Y");
+        //angle += moveMouseVertical * mouseSenY;
+        //angle = Mathf.Clamp(angle, -90, 90);
+        //cam.transform.rotation = Quaternion.Euler (angle, cam.transform.rotation.y, 0);
+
+        float rotation = -moveMouseVertical * mouseSenY;
+        if (cam.transform.localEulerAngles.x < 180 && cam.transform.localEulerAngles.x + rotation > 90)
+        {
+            rotation = 90 - cam.transform.localEulerAngles.x;
+        }
+        if (cam.transform.localEulerAngles.x > 180 && cam.transform.localEulerAngles.x + rotation < 270)
+        {
+           rotation = 270 - cam.transform.localEulerAngles.x;
+        }
+        transform.Rotate(new Vector3(rotation, 0.0f, 0.0f));
+    }
+}
---
+++ The_Battle_Arena/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public float mouseSen;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveMouseHorizontal = Input.GetAxis("Mouse X");
+
+        transform.Translate(new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed));
+
+        transform.Rotate(new Vector3(0.0f, moveMouseHorizontal * mouseSen, 0.0f));
+
+
+
+    }
+}
