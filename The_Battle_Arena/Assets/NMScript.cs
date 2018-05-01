b94ebdae79ab7cafb19fc5c413eddf08f221ab80
jixuanwang <jasonw96@gmail.com>
2018-05-01 14:02:55 -0400

    Added FPS, Basic Teams

---
+++ The_Battle_Arena/Assets/NMScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NMScript : NetworkManager {
+
+    private static int playerNumber = 0;
+
+    //https://answers.unity.com/questions/1063433/unet-proper-way-to-set-player-team-onserveraddplay.html
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player = (GameObject)Instantiate(playerPrefab, new Vector3(0,0,0), new Quaternion(0,0,0,0));
+        player.GetComponent<PlayerController>().team = playerNumber % 2;
+        if(playerNumber < 2)
+        {
+            player.GetComponent<PlayerController>().commander = true;
+        }
+        playerNumber++;
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+}
--- The_Battle_Arena/Assets/PlayerController.cs
+++ The_Battle_Arena/Assets/PlayerController.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
+//https://github.com/Brackeys/MultiplayerFPS-Tutorial/blob/master/MultiplayerFPS/Assets/Scripts/PlayerMotor.cs
 public class PlayerController : NetworkBehaviour
 {
+    [SyncVar]
+    public int team = 0;
+    [SyncVar]
+    public bool commander = false;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+
+    private Vector3 rotation = Vector3.zero;
+    public float cameraRotationLimit = 85f;
+    private float cameraRotationX = 0f;
+    private float currentCameraRotationX = 0f;
 
     void Update()
     {
@@ -12,7 +22,7 @@ public class PlayerController : NetworkBehaviour
         {
             return;
         }
-
+       
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
@@ -23,6 +33,43 @@ public class PlayerController : NetworkBehaviour
         {
             CmdFire();
         }
+
+        if (team == 0)
+        {
+            CmdColorChange(transform.gameObject, Color.red);
+        }
+        else
+        {
+            CmdColorChange(transform.gameObject, Color.blue);
+        }
+
+        // https://forum.unity.com/threads/how-do-i-give-each-player-their-own-camera.63708/
+        // KEEP CAMERA FIRST CHILD OF PLAYER
+        Camera cam = transform.GetChild(0).GetComponent<Camera>();
+        cam.enabled = true;
+        transform.GetChild(0).GetComponent<AudioListener>().enabled = true;
+
+        if (commander)
+        {
+            cam.transform.position = new Vector3(0f, 25f, 0f);
+            cam.transform.LookAt(new Vector3(0f, 0f, 0f));
+        }
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * 3f;
+
+        //Apply rotation
+        rotation = _rotation;
+
+        //Calculate camera rotation as a 3D vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot * 3f;
+
+        //Apply camera rotation
+        cameraRotationX = _cameraRotationX;
+
     }
 
     // This [Command] code is called on the Client …
@@ -48,6 +95,67 @@ public class PlayerController : NetworkBehaviour
 
     public override void OnStartLocalPlayer()
     {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
+
+        //GetComponent<MeshRenderer>().material.color = Color.green;
+        //if (team == 0)
+        //{
+
+        //    CmdColorChange(transform.gameObject, Color.red);
+        //}
+        //else
+        //{
+
+        //    CmdColorChange(transform.gameObject, Color.blue);
+        //}
+        //if (team == 0)
+        //{
+
+        //    GetComponent<MeshRenderer>().material.color = Color.red;
+        //} else
+        //{
+
+        //    GetComponent<MeshRenderer>().material.color = Color.blue;
+        //}
+    }
+    //https://answers.unity.com/questions/1063433/unet-proper-way-to-set-player-team-onserveraddplay.html
+
+    [Command]
+    void CmdColorChange(GameObject obj, Color toChange)
+    {
+        RpcColorChange(obj, toChange);
+    }
+
+    [ClientRpc]
+    void RpcColorChange(GameObject obj, Color toChange)
+    {
+        obj.GetComponent<MeshRenderer>().material.color = toChange;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!commander) {
+            PerformRotation();
+        }
+    }
+
+    //FIX LATER
+    void PerformRotation()
+    {
+        Camera cam = transform.GetChild(0).GetComponent<Camera>();
+
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            // Set our rotation and clamp it
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            //Apply our rotation to the transform of our camera
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
     }
 }
+
+
